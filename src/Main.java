a268bc8cb8b8784f6669e607af78eca28373b72e
annamoskviceva <Annab.spb@mail.ru>
2024-01-17 12:57:43 +0300

    watch

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        watch();
+    }
+    public static void watch() {
+        for (int hour = 0; hour <= 23; hour++){
+            for (int minute = 0; minute <= 59; minute++){
+                for (int seconde = 0; seconde <= 60; seconde++){
+                    System.out.println(hour + " : " + minute + " : " + seconde);
+                }
+            }
+        }
     }
 }
